c3f62dada1b4bb9a8fb6b28e88f02c57dcca38ba
Rajnish Singh <rajnish.kumar2998@gmail.com>
2020-01-24 17:48:24 +0530

    Update dataItems.dart

--- lib/data/dataItems.dart
+++ lib/data/dataItems.dart
@@ -1,22 +1,3 @@
-// UI Design List
-final List<String> resourcesList = ['Fruit UI Design', 'Foody Design', 'Calculator'];
-
-// First UI1 Items List
-List<String> path = [
-  'https://images.pexels.com/photos/161559/background-bitter-breakfast-bright-161559.jpeg?cs=srgb&dl=orange-fruit-161559.jpg&fm=jpg',
-  'https://5.imimg.com/data5/OC/VQ/MY-43969551/jamun-fruit-500x500.png',
-  'https://images.pexels.com/photos/209339/pexels-photo-209339.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-  'https://images.unsplash.com/photo-1554579384-2d90b36072eb?ixlib=rb-1.2.1&auto=format&fit=crop&w=416&h=312&q=60',
-  'https://3.imimg.com/data3/SJ/NJ/MY-9608713/mango-mango-pulp-500x500.png'
-];
-List<String> names = ['Orange', 'Blackberry', 'Apple', 'Strawberry', 'Mango'];
-List<String> price = ['75', '85', '452', '45', '75'];
-
-// Profile Image
-String profilePath =
-    'https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80';
-
-// Foody UI2
 // Image List
 
 List<String> ui2path = [
@@ -27,5 +8,9 @@ List<String> ui2path = [
   'https://avocadopesto.com/wp-content/uploads/2017/05/sorghum-bowls-FG.jpg',
   'https://imageresizer.static9.net.au/EaO9FCpwtt3XnnUdifrm_pUksQU=/600x338/smart/https%3A%2F%2Fprod.static9.net.au%2F_%2Fmedia%2F2017%2F10%2F10%2F12%2F39%2FTassal-spring-poke-bowl.jpg'
 ];
+
+// Images random names
 List<String> ui2names =['Slamon Bowl','Spring Bowl', 'Avocado Bowl', 'Berry Bowl','Haka Bowl','Mac Bowl'];
+
+// Random Price
 List<double> ui2price = [233.5, 343.5, 312.3, 232.1, 2321.2, 1232.3];
